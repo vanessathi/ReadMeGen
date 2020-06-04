d7800269e28c986c14e8f22d7b1ca936cb882bc3
Vanessa Pham <phamxvanessa@gmail.com>
2020-06-04 02:39:26 -0600

    added questions

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
-const questions = [
+var inquirer = require('inquirer');
+var fs = require('fs');
+const path = require('path')
+const generateMarkdown = require('./generateMarkdown');
 
-];
+const questions = [
+        {
+            type: "input",
+            message: "What is your Github username?",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "What is your e-mail?",
+            name: "email"
+        },
+
+        {
+            type: "input",
+            message: "What is the name of your project?",
+            name: "projectName"
+
+        },
+        {
+            type: "input",
+            message: "Enter a description of your project.",
+            name: "description"
+
+        },
+        {
+            type: "input",
+            message: "Who all has contributed to this project?",
+            name: "contribute"
+
+        },
+        {
+            type: "input",
+            message: "What command should run the file?",
+            name: "runFile"
+
+        }
+    ]
+
 
 function writeToFile(fileName, data) {
-}
-
-function init() {
-
-}
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  }
+  
+  function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+      writeToFile("README.md", generateMarkdown({ ...inquirerResponses }));
+    })
+  }
 
 init();
