a0bd06aca8838a8b0430b2ac370af401d36f72b3
Vanessa Pham <phamxvanessa@gmail.com>
2020-06-04 02:53:52 -0600

    add movie and screenshots

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var inquirer = require('inquirer');
-var fs = require('fs');
+const inquirer = require('inquirer');
+const fs = require('fs');
 const path = require('path')
 const generateMarkdown = require('./generateMarkdown');
 
